38ac9000d6b1fdcc70d552401a279a0d6066e603
Nicklas Utgaard <nutgaard@gmail.com>
2014-03-26 12:15:03 +0100

    Updated make scripts

--- literature_study/make.sh
+++ literature_study/make.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+#If unexpected EOF try running 'dos2unix make.sh' command
 echo -e "\e[1m\e[34mBuilding latex\e[0m\e[39m"
 latexmk -gg -silent -bibtex -pdf Literature_study.tex | grep -i warning
 echo -e "\e[1m\e[34mBuild complete\e[0m\e[39m"
+
+if [ ! $# -eq 0 ]; then
+        if [ "$1" = "publish" ]; then
+                read -p "What is your NTNU username:" un
+                scp Literature_study.pdf $un@login.stud.ntnu.no:public_html/Literature_study.pdf
+        fi
+fi
--- make.sh
+++ make.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+#If unexpected EOF try running 'dos2unix make.sh' command
+
 echo -e "\e[1m\e[34mBuilding latex\e[0m\e[39m"
 latexmk -gg -silent -bibtex -pdf Masterprosjekt.tex | grep -i warning
 echo -e "\e[1m\e[34mBuild complete\e[0m\e[39m"
